09d357d3cd4ecc110f4e6781995ebb45a54f7ea4
Sachin Kale <111455535+Sachinkale11@users.noreply.github.com>
2025-04-05 00:57:41 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignupPage';
+import SignUpPage from './pages/SignUpPage';
 import Footer from './components/Footer';
 import { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
